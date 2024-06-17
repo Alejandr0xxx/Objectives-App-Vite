5571545696f6f118842659981056c3761c8c82d4
Alejandr0x <ytalejandrovevo@gmail.com>
2024-06-16 21:16:50 -0500

    required added

--- src/components/createNew/objectiveDetail.jsx
+++ src/components/createNew/objectiveDetail.jsx
@@ -1,41 +1,29 @@
 import { useState } from 'react';
 import styles from './details.module.css'
-import {  redirect, useFetcher, useNavigate } from 'react-router-dom';
+import { redirect, useFetcher, useNavigate } from 'react-router-dom';
 import { createObj } from '../functions';
 
-export async function action({request}){
+export async function action({ request }) {
     const formData = await request.formData();
     const data = Object.fromEntries(formData.entries());
-    if(data.completedTimes === undefined) data.completedTimes = 0; 
+    if (data.completedTimes === undefined) data.completedTimes = 0;
+    if (Number(data.completedTimes) >= Number(data.objective)) { return alert('Completed times MUST NO be higher than the objective!   '), null }
     await createObj(data);
     return redirect('/list')
-}   
+
+}
 
 function Details() {
 
     const navigate = useNavigate()
     const fetcher = useFetcher();
-    const [completedIn, setCompletedIn] = useState(false);
-
-    const [form, setForm] = useState({
-        details: '',
-        events: 1,
-        frecuency: 'daily',
-        icon: '💻',
-        objective: 1,
-        deadline: '2025-01-01',
-        completed: false,
-        completedTimes: 1,
-    })
-
-    const onChange = (event, prop) => {
-        setForm((state) =>({...state, [prop]: event.target.value}))
-        let completed = prop === 'completed'
-        if (completed) {
-            setCompletedIn(event.target.value === 'true');
+    const [isCompleted, setisCompleted] = useState(false)
+    const onChange = (event) => {
+        if (event.target.name === 'completed') {
+            setisCompleted(event.target.value === 'true');
         }
     }
-    const { details, events, frecuency, icon, objective, deadline, completedTimes } = form
+
     const frecuencies = ['daily', 'weekly', 'monthly', 'yearly'];
     const icons = ['💻', '✈️', '🏃', '📚', '🍗'];
 
@@ -44,47 +32,47 @@ function Details() {
             <div className='p-4'>
                 <label className="label">
                     Describe your objective
-                    <input className='input' type="text" placeholder="e.g. 52 walks" name='details' value={details} onChange={(e => onChange(e, 'details'))} autoFocus />
+                    <input className='input' type="text" placeholder="e.g. 52 walks" name='details' autoFocus required />
                 </label>
                 <label className='label'>
                     How often do you hope to achieve your objective? (e.g. once a week)
                     <div className="flex">
-                        <input className='input mr-6' type="number" name='events' value={events} onChange={(e => onChange(e, 'events'))} />
-                        <select className='input' name='frecuency' value={frecuency} onChange={(e => onChange(e, 'frecuency'))}>
+                        <input className='input mr-6' type="number" name='events' defaultValue='1' required />
+                        <select className='input' name='frecuency' required>
                             {frecuencies.map(frecuencies => <option key={frecuencies} value={frecuencies}>{frecuencies}</option>)}
                         </select>
                     </div>
                 </label>
                 <label className="label">
                     How many times do you hope to achieve you objective?
-                    <input className='input' type="number" placeholder="7" name='objective' value={objective} onChange={(e => onChange(e, 'objective'))} />
+                    <input className='input' type="number" name='objective' defaultValue={1} required />
                 </label>
                 <label className="label">
                     Do you have a deadline?
-                    <input className='input' type="date" name='deadline' value={deadline} onChange={(e => onChange(e, 'deadline'))} />
+                    <input className='input' type="date" name='deadline' defaultValue={'2025-01-01'} required />
                 </label>
                 <label className="label">
                     Have you completed this objetive before?
                     <label className="label">
-                        <input onChange={(e => onChange(e, 'completed'))}  value={true} name="completed" type="radio" />
+                        <input className='mt-1' onChange={(e) => onChange(e)} value={true} name="completed" type="radio" required />
                         Yes
                     </label>
                     <label className="label">
-                        <input onChange={(e => onChange(e, 'completed'))} value={false} name="completed" type="radio" />
+                        <input onChange={(e) => onChange(e)} value={false} name="completed" type="radio" required />
                         No
                     </label>
                 </label>
-                {completedIn &&
+                {isCompleted &&
                     <>
                         <label className='label'>
                             How many times have you completed this objective before?
-                            <input  onChange={(e => onChange(e, 'completedTimes'))}  name='completedTimes' value={completedTimes} type="number" className='input'/>
+                            <input name='completedTimes' defaultValue={1} type="number" className='input' required />
                         </label>
                     </>
                 }
                 <label className="label">
                     Choose an icon
-                    <select className='input' name='icon' value={icon} onChange={(e => onChange(e, 'icon'))} >
+                    <select className='input' name='icon' required>
                         {icons.map(icons => <option key={icons} value={icons}>{icons}</option>)}
                     </select>
                 </label>
@@ -96,5 +84,6 @@ function Details() {
         </fetcher.Form>
     );
 }
+
 
 export default Details;
